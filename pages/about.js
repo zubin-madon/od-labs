33d92483268020cc5442ff4ba16c54ac27ec99f5
zubin-madon <madon.zubin@gmail.com>
2022-07-18 10:58:04 +0530

    about

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,34 @@
 
 function About() {
   return (
-    <div className="granpa bg-green-300 relative object-cover h-screen w-screen">
-    <div className="parent bottom-1/2 right-1/2 bg-red-500 absolute">
-        <h1>Parent</h1>
-    </div>
+    <div className='parent relative'>
+    <video
+        src={"/videos/about.mp4"}
+        autoPlay
+        muted
+        loop
+        className="object-contain lg:object-cover h-screen w-screen absolute rotate-90 xl:rotate-0 z-0"
+      />
+     
 
-    <div className="child absolute bottom-10 right-10">
-        <h1>Child</h1>
-    </div>
-    
-    </div>
+      <svg
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 1920 1080"
+        className="object-cover h-screen w-screen absolute z-10 opacity-0 rotate-90 lg:rotate-0"
+      >
+        <image
+          width="1920"
+          height="1080"
+          xlinkHref="/images/transition-a-hidden.png"
+          className='rotate-90 lg:rotate-0 opacity-0'
+        ></image>
+        <a xlinkHref="/home">
+    <rect x="-250" y="735" fill="#fff" opacity="0" width="433" height="273"></rect>
+  </a>
+      </svg>
+</div>
   )
 }
 
